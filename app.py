1f42ff7e141d8adb7ebead43b54b01c55961522a
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-04-15 10:34:08 +0530

    Fix Scraper

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ from scraper import data_scraper
 
 app = Flask(__name__)
 
-ranks, names,links, worths, countries, industries = data_scraper()
+TOTAL_PERSONS = 500
+
+ranks, names, links, worths, last_changes, ytds, countries, industries = data_scraper(TOTAL_PERSONS)
 response_dict = {}
-for i in range(100):
+for i in range(TOTAL_PERSONS):
     temp_dict = {
         "rank": ranks[i], 
         "name": names[i], 
         "link": links[i],
         "worth": worths[i],
         "country": countries[i], 
+        "last_changes": last_changes[i],
+        "ytds": ytds[i],
         "industry": industries[i]
     }
     response_dict[i+1] = temp_dict
@@ -29,8 +33,8 @@ def hello_world():
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=response_dict)
+    return render_template('index.html', data=response_dict, num=TOTAL_PERSONS)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -1,49 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
-def data_scraper():
+BROWSER = webdriver.Chrome(executable_path="chromedriver.exe")
 
-    header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Referer": "https://www.bloomberg.com/",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Pragma": "no-cache"
-    }
 
-    response = requests.get(
-        "https://www.bloomberg.com/billionaires/", headers=header)
+def data_scraper(num):
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+    BROWSER.get("https://www.bloomberg.com/billionaires/")
+    html_source = BROWSER.page_source
+    BROWSER.close()
 
-    response_rank = soup.find_all('div', attrs={'table-cell t-rank'})
-    ranks = [rank.get_text().strip() for rank in response_rank][:100]
+    soup = BeautifulSoup(html_source, 'html.parser')
 
-    response_name = soup.find_all('div', attrs={'table-cell t-name'})
-    names = [name.get_text().strip() for name in response_name][:100]
+    response_rank = soup.find_all('div', class_='table-cell t-rank')
+    ranks = [rank.get_text().strip() for rank in response_rank][:num]
+
+    response_name = soup.find_all('div', class_='table-cell t-name')
+    names = [name.get_text().strip() for name in response_name][:num]
 
     links = []
-    for div in soup.findAll('div', attrs={'table-cell t-name'}):
-        links.append((div.find('a')['href']).replace("./",""))
+    for div in soup.findAll('div', class_='table-cell t-name'):
+        links.append((div.find('a')['href']).replace("./", ""))
 
-    response_worth = soup.find_all('div', attrs={'table-cell active t-nw'})
-    worths = [worth.get_text().strip() for worth in response_worth][1:101]
+    response_worth = soup.find_all('div', class_='table-cell active t-nw')
+    worths = [worth.get_text().strip() for worth in response_worth][1:num+1]
 
-    # response_last_change = soup.find_all('div', attrs={'table-cell t-lcd pos'})
-    # last_changes = [change.get_text().strip()
-    #                 for change in response_last_change][:100]
+    response_last_change = soup.find_all('div', class_='t-lcd')
+    last_changes = [change.get_text().strip()
+                    for change in response_last_change][1:num+1]
 
-    # response_ytd = soup.find_all('div', attrs={'table-cell t-ycd pos'})
-    # ytds = [ytd.get_text().strip() for ytd in response_ytd][:100]
+    response_ytd = soup.find_all('div', class_='t-ycd')
+    ytds = [ytd.get_text().strip() for ytd in response_ytd][1:num+1]
 
-    response_country = soup.find_all('div', attrs={'table-cell t-country'})
+    response_country = soup.find_all('div', class_='table-cell t-country')
     countries = [country.get_text().strip()
-                 for country in response_country][1:101]
+                 for country in response_country][1:num+1]
 
-    response_industry = soup.find_all('div', attrs={'table-cell t-industry'})
+    response_industry = soup.find_all('div', class_='table-cell t-industry')
     industries = [industry.get_text().strip()
-                  for industry in response_industry][1:101]
-    print(ranks)
-    return ranks, names, links[:100], worths, countries, industries
+                  for industry in response_industry][1:num+1]
+    return ranks, names, links[:num], worths, last_changes, ytds, countries, industries
+
+
+if __name__ == '__main__':
+    ranks, names, links, worths, last_changes, ytds, countries, industries = data_scraper()
